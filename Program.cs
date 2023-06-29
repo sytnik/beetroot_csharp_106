c49a8f35377b20c5e4b15b0c678fd68c755ce6e1
Oleh Sytnik <osyt@telenor.dk>
2023-06-29 21:26:35 +0300

    lesson 6 code added

--- Program.cs
+++ Program.cs
@@ -1,224 +1,363 @@
-﻿class Program
+﻿namespace Lesson5;
+
+enum Categories
 {
-    private int SomeClassValue = 5;
+    Electronics = 1,
+    Food = 5,
+    Automotive = 6,
+    Arts = 10,
+    BeautyCare = 11,
+    Fashion = 15,
+    WomanFashion = 15
+}
 
-    static string ConcatOutput(string title, int val = 1, params string[] words)
+class Program
+{
+    public static int[] ShrinkArray(int[] original, int newSize)
     {
-        string head = title + ": ";
-        foreach (var word in words) head += word + ", ";
-        return head;
-    }
-
-    static string PrintValue(int val, string str, out int res, ref int someValue, int opt = 1)
-    {
-        var result = "Some value " + val + str + opt;
-        Console.WriteLine(result);
-        val = val + 1;
-        someValue = opt + someValue + 2;
-        res = opt + 1;
-        return res > 5 ? result : "";
-    }
-
-    static string PrintValue(int val, string str, out int res, ref int someValue,
-        double nextValue, int opt = 1)
-    {
-        var result = "Some value " + val + str + opt;
-        Console.WriteLine(result);
-        val = val + 1;
-        someValue = opt + someValue + 2;
-        res = opt + 1;
-        return res > 5 ? result : "";
-    }
-
-    static int Sum(int from, int to)
-    {
-        // var local = SomeClassValue;
-        if (from > to) return Sum(to, from);
-        if (from == to) return from;
-        return from + Sum(from + 1, to);
-    }
-
-    static int SumFor(int from, int to)
-    {
-        var result = 0;
-        for (var i = from; i <= to; i++)
-        {
-            result += i;
-        }
-
-        return result;
-    }
-
-    static void PrintValueShort() => Console.WriteLine("Some value");
-
-    static unsafe void Pointers()
-    {
-        int x = 42;
-        int* p = &x;
-        Console.WriteLine("Value of x: " + x);
-        Console.WriteLine("Value stored in the pointer p: " + *p);
-        *p = 24; // Modifying the value indirectly through the pointer
-        Console.WriteLine("New value of x: " + x);
+        var newArray = new int[newSize];
+        Array.Copy(original, original.Length - newSize, newArray, 0, newSize);
+        return newArray;
     }
 
     static void Main()
     {
-        Pointers();
-        int val1 = 1;
-        object boxed = val1; // boxing
-        val1 = (int) boxed; // unboxing
-        int? val = null;
-        // var test = val.Value;
-        int notNull = 0;
-        var copy = val.HasValue ? val.Value : 0;
-        var copy1 = val ?? 5;
-        var copy2 = notNull != 0 ? notNull : 7;
-        Console.WriteLine("Input some int...");
-        var stringInput = Console.ReadLine();
-        // var int1 = Convert.ToInt32(stringInput);
-        if (int.TryParse(stringInput, out var int1))
-            Console.WriteLine($"Input was valid, {int1}");
-        else Console.WriteLine("Input was not valid");
-        var title = "title";
-        var concat1 = ConcatOutput(title);
-        var concat2 = ConcatOutput("title", 1, "w1", "w2", "w3", "w4");
-        Sum(5, 10);
-        SumFor(5, 10);
-        int i = 0;
-        var someNonRef = 5;
-        var intermediate = 1;
-        var result = PrintValue(someNonRef, str: "string", out var x, ref intermediate, 5);
-        var result2 =
-            PrintValue(someNonRef, str: "string", out var y, ref intermediate, 5, 5);
-        Console.WriteLine(x);
-        PrintValueShort();
-        // for (int i = 0; i < 5; i++)
-        // {
-        //     Console.Write($"Iteration {i}: "); // виводиться завжди
-        //     if (i < 3)
-        //     {
-        //         Console.WriteLine("skip");
-        //         continue; // при 0, 1, 2 робимо пропуск наступного рядку
-        //     }
-        //     Console.WriteLine("done"); // при 3, 4 виводимо done
-        // }
-        //
-        // var str = "qwerty";
-        // foreach (int ch in str)
-        // {
-        //     if (ch == 'r') 
-        //     { // в кейсі літери ‘r’ виходимо з циклу
-        //         break;
-        //     }
-        //     Console.Write($"{ch}"); // ‘q’,’w’,’e’
-        // }
+        // get element by emum
+        var fashion = Categories.Fashion;
+        var womanFashion = (int) Categories.WomanFashion;
+        // by int
+        var elem = 15;
+        var enumVal = (Categories) Enum.Parse(typeof(Categories), elem.ToString());
+// init array
+        var strings = new string[5];
+        var elems = new int[5];
+        // int[] elems2 = {1, 2, 3, 4, 5};
+        var elems0 = new[] {1, 2, 3, 4, 5};
 
-        // while
-        // int i = 0;
-        // while (i < 5) // while умова true
-        // {
-        //     Console.WriteLine(i);         // тіло циклу
-        //     i++;			//
-        // }
-        //
-        // // do while
-        // i = 0;
-        // do 
-        // {
-        //     Console.WriteLine(i++); // перша ітерація виконається
-        // }
-        // while (i < 5);
+        for (var i = 0; i < elems0.Length - 1; i++)
+            elems0[i] += 1;
+        foreach (var el in elems0)
+            Console.WriteLine(el);
+        var person =
+            new Tuple<int, string, string>(1, "Steve", "Jobs");
+        person = Tuple.Create(2, person.Item2, person.Item3);
+        var person1 = Tuple.Create(1, "Steve", "Jobs");
+        PrintTuple(person1);
+        var persons = new[] {person, person1};
+        var person2 = (Id: 1, FirstName: "Bill", LastName: "Gates");
+        person2.Id = 3;
+        PrintValueTuple(person2);
 
-        // var str = "qwerty";
-        // foreach (var ch in str)
-        // {
-        //     Console.Write(str.IndexOf(ch));  
-        // } // ‘qwerty’
+        void PrintTuple(Tuple<int, string, string> tuple) =>
+            Console.WriteLine(tuple.Item1 + " " + tuple.Item2);
 
-        // var str = "qwerty";
-        //
-        // // for (ініціалізація; умова; інкремент)
-        // for (var i = 0; i < str.Length; i++)
-        // {
-        //     Console.Write(str[i]);
-        // } // ‘qwerty’
-        //
-        // // foreach (type variable in collection)
-        // foreach (var ch in str)
-        // {
-        //     Console.Write(ch);  
-        // } // ‘qwerty’
+        void PrintValueTuple((int Id, string Name, string) tuple) =>
+            Console.WriteLine(tuple.Id + " " + tuple.Name);
 
-        // int v;
-        // int a = int.Parse(Console.ReadLine());
-        // // string a, b;
-        // int b;
-        // if (a == 1)
-        // {
-        //     b = 10;
-        // }
-        // else
-        // {
-        //     b = 15;
-        // }
-        //
-        // b = a == 1
-        //     ? 10 // true
-        //     : 15; // false
+        string qwerty = "qwerty";
+        var q = qwerty[1];
+        char[] qwertyArrayGeneric = qwerty.ToArray();
+        char[] qwertyArray = qwerty.ToCharArray();
+        
+        ShrinkArray(elems0, 2);
+        var third = elems0[2];
+        var last = elems0[elems0.Length - 1];
+        var rangeFirst = elems0[0..0];
+        var first = elems0[0];
+        var sec = elems0[1];
+        var last1 = elems0[^1];
+        var secToLast = elems0[^2];
+        elems0[4] = 10;
+        Array.Resize(ref elems0, 8);
+        elems0[5] = 15;
+        Array.Resize(ref elems0, 4);
+
+        int[] source = {5, 8, 6, 7, 3, 35, 4};
+        var sorted1 = Sort(SortAlgorithmType.Selection, OrderBy.Asc, source);
+        // SampleArray();
+        MultidimensionalArray();
+        JaggedArray();
+        TupleTest();
+        // LINQ
+        // [] - array of some type
+        var chArr = new[] {'a', 's', 'd'};
+
+        var str = string.Join(',', source);
+        var str1 = string.Join(",*, ", source);
+        var str2 = string.Join("", chArr);
+
+        Console.WriteLine(string.Join(", ", sorted1));
+        var sorted2 = Sort(SortAlgorithmType.Bubble, OrderBy.Desc, source);
+        Console.WriteLine(string.Join(", ", sorted2));
+        var sorted3 = Sort(SortAlgorithmType.Insertion, OrderBy.Asc, 5, 8, 6, 7, 3, 35, 4);
+        var sorted4 = Sort(SortAlgorithmType.Insertion, OrderBy.Asc, new[] {5, 8, 6, 7, 3, 35, 4});
+        Console.WriteLine(string.Join(", ", sorted3));
+        var t = TwoBeforeLastByRange(1, 2, 3, 4, 5);
+        var a = Sort3d();
+    }
+
+    static int[,,] Sort3d()
+    {
+        // sample 3x4x5 array
+        var myArray = new int[3, 4, 5]
+        {
+            {{6, 5, 4, 3, 2}, {1, 2, 3, 4, 5}, {6, 5, 4, 3, 2}, {1, 2, 3, 4, 5}},
+            {{9, 8, 7, 6, 5}, {4, 5, 6, 7, 8}, {9, 8, 7, 6, 5}, {4, 5, 6, 7, 8}},
+            {{3, 2, 1, 0, 9}, {8, 7, 6, 5, 4}, {3, 2, 1, 0, 9}, {8, 7, 6, 5, 4}}
+        };
+        // create an one-dimensional temp array
+        var flatArray = new int[myArray.Length];
+        // transfer the 3d array to 1d
+        Buffer.BlockCopy(myArray, 0, flatArray,
+            0, sizeof(int) * myArray.Length);
+        // sort 1d array
+        Array.Sort(flatArray);
+        // transfer the sorted 1d array back to the 3d one
+        Buffer.BlockCopy(flatArray, 0, myArray,
+            0, sizeof(int) * myArray.Length);
+        return myArray;
+    }
 
 
-        // v = int.Parse(Console.ReadLine());
-        // switch (v)
-        // {
-        //     case < 10:
-        //         a = "beforeTen";
-        //         break;
-        //     case 10:
-        //         a = "isTen";
-        //         break;
-        //     case > 10 and < 100 or > 1000:
-        //         a = "afterTen";
-        //         break;
-        //     default:
-        //         a = "smth else";
-        //         break;
-        // }
-        //
-        // // switch expression
-        // a = v switch
-        // {
-        //     < 10 => "beforeTen",
-        //     10 => "isTen",
-        //     > 10 and < 100 => "afterTen",
-        //     _ => "smth else"
-        // };
-        //
-        // Console.WriteLine(a);
+    static int[] TwoBeforeLastByRange(params int[] array)
+    {
+        // from array[3] from last to array[1] from last
+        return array[^3..^1];
+    }
 
+    // gereral Sort method
+    static int[] Sort(SortAlgorithmType type, OrderBy order, params int[] source)
+    {
+        switch (type)
+        {
+            case SortAlgorithmType.Selection:
+                return SelectionSort(order, source);
+            case SortAlgorithmType.Bubble:
+                return BubbleSort(order, source);
+            case SortAlgorithmType.Insertion:
+                return InsertionSort(order, source);
+            default:
+                return source;
+        }
+    }
 
-        // b = Console.ReadLine();
-        // bool boolCondition = a == "true";
-        // bool anotherCondition = b == "true";
-        // if (boolCondition && anotherCondition) 
-        // {
-        //     Console.WriteLine("boolCondition is true");
-        // }
-        // else if (anotherCondition) 
-        // {
-        //     Console.WriteLine("boolCondition is not true, but anotherCondition is true");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("boolCondition is not true, and anotherCondition is not true");
-        // }
+    // SelectionSort
+    static int[] SelectionSort(OrderBy order, params int[] source)
+    {
+        var n = source.Length;
+        for (var i = 0; i < n - 1; i++)
+        {
+            var minIndex = i;
+            for (var j = i + 1; j < n; j++)
+            {
+                if (order == OrderBy.Asc && source[j] < source[minIndex])
+                {
+                    minIndex = j;
+                }
+                else if (order == OrderBy.Desc && source[j] > source[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            var temp = source[minIndex];
+            source[minIndex] = source[i];
+            source[i] = temp;
+            
+            // (source[minIndex], source[i]) = (source[i], source[minIndex]);
+        }
+
+        return source;
+    }
+
+    // BubbleSort
+    static int[] BubbleSort(OrderBy order, params int[] source)
+    {
+        var n = source.Length;
+        for (var i = 0; i < n - 1; i++)
+        {
+            for (var j = 0; j < n - i - 1; j++)
+            {
+                if (order == OrderBy.Asc && source[j] > source[j + 1])
+                    (source[j], source[j + 1]) = (source[j + 1], source[j]);
+                else if (order == OrderBy.Desc && source[j] < source[j + 1])
+                {
+                    (source[j], source[j + 1]) = (source[j + 1], source[j]);
+                }
+            }
+        }
+
+        return source;
+    }
+
+    // InsertionSort
+    static int[] InsertionSort(OrderBy order, params int[] source)
+    {
+        var n = source.Length;
+        for (var i = 1; i < n; i++)
+        {
+            var key = source[i];
+            var j = i - 1;
+
+            if (order == OrderBy.Asc)
+            {
+                while (j >= 0 && source[j] > key)
+                {
+                    source[j + 1] = source[j];
+                    j--;
+                }
+            }
+            else if (order == OrderBy.Desc)
+            {
+                while (j >= 0 && source[j] < key)
+                {
+                    source[j + 1] = source[j];
+                    j--;
+                }
+            }
+
+            source[j + 1] = key;
+        }
+
+        return source;
+    }
+
+    enum SortAlgorithmType
+    {
+        Selection,
+        Bubble,
+        Insertion
+    }
+
+    enum OrderBy
+    {
+        Asc,
+        Desc
+    }
+
+    static void SampleArray()
+    {
+        //init array
+        var arr = new int[0];
+        arr = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        // index
+        var element = arr[5];
+        // Index
+        var index = new Index(1);
+        var second = arr[index];
+        //Range
+        var twoElements = arr[0..2];
+        var twoLast = arr[^1..^0];
+        var two = arr[^2..];
+        var two1 = arr[^2..];
+        var two2 = arr[^3..2];
+        // first elem as new array
+        var range1 = new Range(0, 1);
+        var firstButArr = arr[range1];
+        // take four first
+        var range2 = new Range(new Index(0), 5);
+        var firstFour = arr[range2];
+    }
+
+    static void MultidimensionalArray()
+    {
+        // 2-dimensional - table
+        // 0.0 0.1
+        // 1.0 1.1
+        // 2.0 2.1
+        // 0 0
+        var arr = new int[4, 2];
+        //3-dimensional - cube
+        var array1 = new int[4, 2, 3];
+        // Two-dimensional array.
+        var array2D = new int[,] {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+        // The same array with dimensions specified.
+        var array2Da = new int[4, 2] {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+    }
+
+    static void JaggedArray()
+    {
+        // 0 0 0 0 0
+        // 0 0 0 0
+        // 0 0     
+        // jagged array with 3 different rows
+        var jaggedArray = new int[3][];
+        jaggedArray[0] = new int[5];
+        jaggedArray[1] = new int[4];
+        jaggedArray[2] = new int[2];
+        // init array
+        int[][] jaggedArray2 = new int[][]
+        {
+            new int[] {1, 3, 5, 7, 9},
+            new int[] {0, 2, 4, 6},
+            new int[] {11, 22} // 2
+        };
+        var secondRow = jaggedArray2[1];
+        var val = jaggedArray2[2][1];
+
+        var oneElement = 100;
+        var oneElementArray = new[] {100};
+        var value = oneElementArray[0]; // 100
+    }
+
+    static void TupleTest()
+    {
+        // tuple (not the valuetuple !) - кортеж
+        // with type
+        var person = new Tuple<int, string, string>(1, "Steve", "Jobs");
+
+        // create from arguments
+        var person1 = Tuple.Create(1, "Steve", "Jobs");
+        var x = person1.Item1; // returns 1
+        var y = person1.Item2; // returns "Steve"
+        var z = person1.Item3; // returns "Jobs"
+        // person1.Item1 = 3;
+
+        // valuetuple - кортеж значень (.net fw 4.7+ or System.ValueTuple)
+        // long notation
+        ValueTuple<int, string, string> person4 = (1, "Bill", "Gates");
+        var q = person4.Item1; // returns 1
+        var w = person4.Item2; // returns "Bill"
+        var e = person4.Item3; // returns "Gates"
+
+        // new notation
+        var person2 = (1, "Bill", "Gates");
+        //equivalent Tuple
+        var person3 = Tuple.Create(1, "Bill", "Gates");
+
+        // big valuetuple
+        var numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+
+        // named tuple
+        (int Id, string FirstName, string LastName) person5 = (1, "Bill", "Gates");
+        var a = person5.Id; // returns 1
+        var s = person5.FirstName; // returns "Bill"
+        var d = person5.LastName; // returns "Gates"
+        person5.FirstName = "other";
+
+        // var tuple
+        // var - compile type!
+        // simple init
+        var sometuple = (1, "someText", Guid.NewGuid(), 1.34);
+        // var text = sometuple[1]; // no index
+        var text = sometuple.Item2;
+        // named tuple initialization
+        // GUID (globally unique identifier)
+        var someDouble = 1.34;
+        (int id, string textField, Guid someGuid, double doubleVal) tuple2 =
+            (1, "someText", Guid.NewGuid(), someDouble);
+        var val1 = tuple2.someGuid;
+
+        // named tuple (userId, firstName, isActive)
+        // [] of tuples
+        (int userId, string firstName, bool isActive)[] users =
+            new (int userId, string firstName, bool isActive)[]
+            {
+                (1, "John", true),
+                (2, "Jane", true),
+                (3, "Hanz", false)
+            };
+        var secondName = users[1].firstName; //Jane
+        var thirdUserIsActive = users[2].isActive; //false
     }
 }
-
-// Console.WriteLine("Hello, World!");
-
-// testfunc();
-//
-// static void testfunc()
-// {
-//     int i = 0;
-// }
