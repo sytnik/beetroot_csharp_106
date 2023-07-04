83f976512a3e41169cc118d1fb48f40de82fe147
Oleh Sytnik <osyt@telenor.dk>
2023-07-04 21:46:23 +0300

    lesson 8 code added

---
+++ Person.cs
@@ -0,0 +1,13 @@
+﻿namespace beetroot_csharp_106;
+
+class Person
+{
+    string Name;
+    int Age { get; set; }
+    
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,197 +1,213 @@
-﻿using System.Text;
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace beetroot_csharp_106;
 
-class Program
+public class Program
 {
-    static void Main()
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        List<char> chs = DuplicateUsingList("hello world");
-        char[] charArr = Duplicate("hello world");
-        StrInterpolation();
-    }
+        // string input = "The quick brown fox jumps over the lazy dog.";
+        // string pattern = "fox";
+        // Match match = Regex.Match(input, pattern);
+        // if (match.Success)
+        // {
+        //     Console.WriteLine("Found '{0}' at position {1}", match.Value, match.Index);
+        // }
+        // else Console.WriteLine("Not found");
+        // string input = "The quick brown fox jumps over the lazy dog.";
+        // string pattern = @"\b\w{4}\b";
+        // MatchCollection matches = Regex.Matches(input, pattern);
+        // foreach (Match match in matches)
+        // {
+        //     Console.WriteLine("Found '{0}' at position {1}", match.Value, match.Index);
+        // }
+        // string input = "The quick brown fox jumps over the lazy dog.";
+        // string pattern = @"\b\w{4}\b";
+        // string replacement = "someword";
+        // string result = Regex.Replace(input, pattern, replacement);
+        // Console.WriteLine(result);
+        // string input = "+38(050)12345";
+        // string input1 = "+38(050)1234567";
+        // string input2 = "+38(050)1234";
+        // string pattern = @"\+38\(0\d{2}\)\d{5,7}"; // +38(0xx)xxxxxxx
+        // // string pattern = @"\b\w{4}\b";
+        // bool isMatch = Regex.IsMatch(input, pattern);
+        // bool isMatch1 = Regex.IsMatch(input1, pattern);
+        // bool isMatch2 = Regex.IsMatch(input2, pattern);
+        // Console.WriteLine(isMatch);
+        // string i1 = "Іваненко І.І.";
+        // string i2 = "Iваненко Іван Іванович";
+        // int firstI = (int) i1[0];
+        // int secI = (int) i2[0];
+        // string pattern1 = @"^[А-ЩЬЮЯҐЄІЇ][а-щьюяґєії']+\s[А-ЩЬЮЯҐЄІЇ]\.[А-ЩЬЮЯҐЄІЇ]\.$"; // ^ - start of the string
+        // Regex regex = new Regex(pattern1);
+        // bool isMatch3 = regex.IsMatch(i1);
+        // bool isMatch4 = regex.IsMatch(i2);
 
-    static void StrInterpolation()
-    {
-        string str1 = "Hello";
-        string str2 = "hello";
-        bool res1 = str1.ToLower() == str2.ToLower();
-        bool result = str1.Equals(str2, StringComparison.OrdinalIgnoreCase);
-        Console.WriteLine(result); // виведе true
+//         string email = "example@gmail.com";
+//         // var simplePattern = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b";
+//         string pattern = @"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+//         Regex regex = new Regex(pattern);
+//         bool isMatch = regex.IsMatch(email);
+//         Console.WriteLine(isMatch); // виведе "True”
+//
+// Console.WriteLine();
 
-        var ch = 'a'.ToString();
-        string someText = "This is a var of:";
-        var r1 = someText[5..];
-        var r2 = someText[5..10];
-        var sub1 = someText.Substring(5);
-        var sub2 = someText.Substring(5, 5);
-        var f1 = someText.IndexOf('i');
-        var f2 = someText.IndexOf("va");
-        var f3 = someText.LastIndexOf("a", StringComparison.Ordinal);
-        var rp1 = someText
-            .Replace('i', 'x')
-            .Replace("ar", "bx");
-        var strtoTrim1 = "   " + someText + "  ";
-        var strtoTrim = $"   {someText}  ";
-        var tr1 = strtoTrim.TrimStart();
-        var tr2 = strtoTrim.TrimEnd();
-        var tr3 = strtoTrim.Trim();
-        var rp2 = someText.Replace("ar", "bx");
-        var nl = $"test\r\ntest{Environment.NewLine}test";
-        someText += "123";
-        int vari = 15000;
-        string endText = "is ready";
-        // concatenation
-        string concat = someText + " " + vari + " " + endText;
-        // interpolation
-        string inter = $"{someText} and text {vari} {endText}";
-        // stringbuilder
-        StringBuilder sb = new StringBuilder();
-        sb = sb.Append(someText)
-            .Append(" ")
-            .Append(vari)
-            .Append(" ")
-            .Append(endText);
-        string res = sb.ToString();
-        StringBuilder sb1 = new StringBuilder();
-        sb1 = sb1.AppendJoin(" ", someText, vari, endText);
-        string re1 = sb1.ToString();
-        // join to one string if separator is the same
-        var intList = new List<int>();
-        intList.Add(1);
-        string res2 =
-            string.Join(" ", someText, vari, endText);
+        // string filePath = "C:/example.txt";
+        // string p2 = "../../../1/Text.txt";
+        // if (File.Exists(p2))
+        // {
+        //     Console.WriteLine("The file exists.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("The file does not exist.");
+        // }
+        int a = 0;
+        try
+        {
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
         
-        string res3 =
-            new StringBuilder()
-                .AppendJoin(" ", someText, vari, endText)
-                .ToString();
+        string filePath = "Text1.txt";
+        string[] lines = {"First line",
+            "\r\n",
+            "Second line",
+            Environment.NewLine,
+            "Third line"};
+        File.WriteAllLines(filePath, lines);
+        string fileContents = "This is an example text.";
+        File.AppendAllText(filePath, fileContents);
+        //
+        // string[] lines1 = File.ReadAllLines(filePath);
+        // File.Delete(filePath);
+        // File.Delete("Text12.txt");
+        // WorkWithFile();
+        var persons = new[]
+        {
+            new Person("user1", 25),
+            new Person("user2", 30),
+            new Person("user3", 35)
+        };
+        Serialize(persons);
+        var personsFromFile = Deserialize();
+        Console.WriteLine();
     }
 
-    static bool Compare(string str1, string str2)
+    static void Serialize(Person[] persons)
     {
-        if (str1.Length != str2.Length)
+        using (var fileStream =
+               new FileStream("Person.json",
+                   FileMode.OpenOrCreate))
         {
-            return false;
-        }
-
-        for (int i = 0; i < str1.Length; i++)
-        {
-            if (str1[i] != str2[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    static void Analyze(string input,
-        out int numAlphabetic, out int numDigits, out int numSpecial)
-    {
-        numAlphabetic = 0;
-        numDigits = 0;
-        numSpecial = 0;
-        // char[] arr = input.ToCharArray();
-        // input = arr.ToString();
-        foreach (char c in input)
-        {
-            if (char.IsLetter(c))
-            {
-                numAlphabetic++;
-            }
-            else if (char.IsDigit(c))
-            {
-                numDigits++;
-            }
-            else // if not letter or digit
-            {
-                numSpecial++;
-            }
+            JsonSerializer.Serialize(fileStream, persons);
         }
     }
 
-    static string Sort(string str)
+    public static Person[] Deserialize()
     {
-        // string => char[]
-        char[] chars = str.ToCharArray();
-        // sort array
-        Array.Sort(chars, StringComparer.OrdinalIgnoreCase);
-        // char[] => string
-        str = new string(chars);
-        return str;
+        using (var fileStream =
+               new FileStream("Person.json",
+                   FileMode.OpenOrCreate))
+        {
+            return JsonSerializer // returns Person[]
+                .Deserialize<Person[]>(fileStream);
+        }
     }
 
-    static char[] Duplicate(string str)
+    private static void WorkWithFile()
     {
-        // with resize new char[str.Length]
-        char[] duplicates = new char[0];
-        char[] seen = new char[str.Length];
-        int duplicateIndex = 0;
-        int seenIndex = 0;
-
-        foreach (char c in str)
+        var users = File.ReadAllLines("Text.txt");
+        var newUser = "User3 0971236547";
+        File.AppendAllText("Text.txt",
+            Environment.NewLine);
+        File.AppendAllText("Text.txt", newUser);
+        var newUserArr = new[] {newUser};
+        File.AppendAllText("Text.txt", "\r\n");
+        File.AppendAllLines("Text.txt", newUserArr);
+        var filePath = "Text1.txt";
+        File.Delete(filePath);
+        if (File.Exists(filePath))
         {
-            bool isDuplicate = false;
-            for (int i = 0; i < seenIndex; i++)
-            {
-                if (c == seen[i])
-                {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-
-            if (isDuplicate)
-            {
-                bool isAlreadyAdded = false;
-                for (int i = 0; i < duplicateIndex; i++)
-                {
-                    if (c == duplicates[i])
-                    {
-                        isAlreadyAdded = true;
-                        break;
-                    }
-                }
-
-                if (!isAlreadyAdded) // not true
-                {
-                    Array.Resize(ref duplicates, duplicates.Length + 1);
-                    duplicates[duplicateIndex++] = c;
-                }
-            }
-            else
-            {
-                seen[seenIndex++] = c;
-            }
+            File.Delete(filePath);
+            Console.WriteLine("The file has been deleted.");
         }
-
-        // with resize
-        // Array.Resize(ref duplicates, duplicateIndex);
-
-        return duplicates;
+        else
+        {
+            Console.WriteLine("The file does not exist.");
+        }
     }
 
-    static List<char> DuplicateUsingList(string str)
+    // клас Person (public = доступ без обмежень)
+    public class Person
     {
-        List<char> duplicates = new List<char>(); // 0 elements
-        List<char> seen = new List<char>(); // 0 elements
-        str = str.Replace(" ", ""); // => "str"
-        foreach (char c in str)
+        // поле (field) класу
+        public string Name { get; set; }
+
+        // властивість (property) класу
+        public int Age { get; set; }
+
+        // конструктор (constructor) класу
+        public Person(string name, int age)
         {
-            if (seen.Contains(c) && !duplicates.Contains(c))
-            {
-                duplicates.Add(c); // size +1
-            }
-            else // if is not seen or is in the duplicates list
-            {
-                seen.Add(c);
-            }
+            Name = name;
+            Age = age;
         }
 
-        char[] dupArray = duplicates.ToArray();
-        List<char> dupList = dupArray.ToList();
-        return duplicates;
+        // метод (method) класу
+        public void PrintInfo() => Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+
+    public static void RegexTest()
+    {
+        var input = "The quick brown fox jumps over the lazy dog.";
+        var pattern = "fox";
+        var match = Regex.Match(input, pattern);
+        if (match.Success)
+        {
+            Console.WriteLine("Found '{0}' at position {1}", match.Value, match.Index);
+        }
+
+        input = "The quick brown fox jumps over the lazy dog.";
+        pattern = @"\b\w{4}\b"; // word with 4 letters
+        var matches = Regex.Matches(input, pattern);
+        foreach (Match match1 in matches)
+        {
+            Console.WriteLine($"Found '{match1.Value}' at position {match1.Index}");
+        }
+
+        input = "The quick brown fox jumps over the lazy dog.";
+        pattern = @"\s";
+        var replacement = "_";
+        var result = Regex.Replace(input, pattern, replacement);
+        Console.WriteLine(result);
+
+        input = "The quick brown fox jumps over the lazy dog.";
+        pattern = "fox";
+        var isMatch = Regex.IsMatch(input, pattern);
+        Console.WriteLine(isMatch);
+
+        pattern = @"\+38\(0\d{2}\)\d{7}"; // +38(0xx)xxxxxxx
+        input = "+38(099)1234567";
+        var regex = new Regex(pattern);
+        isMatch = regex.IsMatch(input); // true
+
+        pattern = @"^[А-ЩЬЮЯҐЄІЇ][а-щьюяґєії']+\s[А-ЩЬЮЯҐЄІЇ]\.[А-ЩЬЮЯҐЄІЇ]\.$"; // ^ - start of the string
+        regex = new Regex(pattern);
+
+        input = "Іванов І.П.";
+        isMatch = regex.IsMatch(input); // true
+
+        var email = "example@example.com";
+        pattern = @"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+        regex = new Regex(pattern);
+
+        isMatch = regex.IsMatch(email);
+        Console.WriteLine(isMatch); // виведе "True”
     }
 }
