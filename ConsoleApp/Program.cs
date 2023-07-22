8c350d108dec8fa2dca046f59ea964cfa994e2c3
Oleh Sytnik <oleh.sytnik@gmail.com>
2023-07-22 12:29:09 +0300

    lesson 16 code added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -18,34 +18,89 @@ public class Program
 
     public static void Main(string[] args)
     {
-        var baseClass = new BaseClass {Id = 1};
-        var derivedClass = new DerivedClass {Id = 2, Name = "Name"};
-        var baseList = new List<BaseClass> {baseClass, derivedClass};
-        var elemId = baseList[1].Id;
-        var name = ((DerivedClass) baseList[1]).Name;
-        var equal = derivedClass.Equals(derivedClass);
-        // var comapare = derivedClass.Compare(baseClass, derivedClass);
-        var derivedList = new List<DerivedClass>
-            {new DerivedClass {Id = 1, Name = "Name1"},
-                new DerivedClass {Id = 2, Name = "Name2"}};
-        var derivedReverse =
-            derivedList.OrderByDescending(e => e.Id);
-        IEnumerable<int> ein = MakeCollectionFromParams(1, 2, 3);
-        PrintGenericCollection(new List<int> {1, 2, 3});
-        PrintGenericCollection(new[] {"1", "2", 3.ToString()});
-        int num1 = 1, num2 = 2;
-        string str1 = "str1", str2 = "str2";
-        // Swap(ref num1, ref num2);
-        Swap(ref str1, ref str2);
-        var stack = new GenericStack<int>();
+        var list = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        list.Add(1);
+        list.AddRange(list);
+        list.Remove(1);
+        list.RemoveAt(0);
+        list.RemoveRange(0, 2);
+        list.RemoveAll(x => x > 4);
+        var contains = list.Contains(1);
+
+        // var dict1 = new Dictionary<int, string>();
+        // dict1.Add(1, "one");
+        // dict1.Add(2, "two");
+        // dict1.Add(3, "three");
+        var dict = new Dictionary<int, string>
+        {
+            {1, "one"},
+            {2, "two"},
+            {3, "three"}
+        };
+        var containsKey = dict.ContainsKey(1);
+        var containsValue = dict.ContainsValue("one");
+        var keys = dict.Keys;
+        var values = dict.Values;
+        var count = dict.Count;
+        dict.Remove(1);
+        dict.Add(4, "four");
+        var try1 = dict.TryAdd(5, "five");
+        var try2 = dict.TryAdd(5, "five");
+        var tryval = dict.TryGetValue(5, out var val);
+        var tryval1 = dict.TryGetValue(7, out var val1);
+        var queue = new Queue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        var dequeue = queue.Dequeue();
+        var peek = queue.Peek();
+        var stack = new Stack<int>();
         stack.Push(1);
         stack.Push(2);
-        var value = stack.Pop();
-        var strStack = new GenericStack<string>();
-        strStack.Push("str1");
-        strStack.Push("str2");
-        var strValue = strStack.Pop();
+        stack.Push(3);
+        var pop = stack.Pop();
+        var peek1 = stack.Peek();
+        var hashset = new HashSet<int>
+        {
+            1,
+            2,
+            3,
+            4,
+            5,
+            6
+        };
+        var hashset1 = new HashSet<int>
+        {
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+        };
+        var intersect = hashset.Intersect(hashset1).ToHashSet();
+        // hashset.IntersectWith(hashset1);
+        var union = hashset.Union(hashset1).ToHashSet();
+        // hashset.UnionWith(hashset1);
+        var except = hashset.Except(hashset1).ToList();
+        hashset.ExceptWith(hashset1);
+        var contains1 = hashset.Contains(1);
+        var contains2 = hashset.Contains(4);
+        var count1 = hashset.Count;
 
-        Console.ReadKey();
+        var linkedList = new LinkedList<int>();
+        linkedList.AddFirst(1);
+        linkedList.AddLast(2);
+        linkedList.AddLast(3);
+        linkedList.AddFirst(11);
+        linkedList.AddLast(4);
+        linkedList.AddLast(5);
+        linkedList.AddAfter(linkedList.Find(3), 10);
+        linkedList.AddBefore(linkedList.Find(3), 20);
+        linkedList.Remove(3);
+        linkedList.RemoveFirst();
+        linkedList.RemoveLast();
+        linkedList.Remove(11);
+        
     }
 }
