96355e61fd1ee44534fcdcfe1935f1a65ead7eeb
Oleh Sytnik <oleh.sytnik@gmail.com>
2023-08-31 20:49:12 +0300

    lesson 30 code added

--- MvcApplication/Program.cs
+++ MvcApplication/Program.cs
@@ -1,9 +1,11 @@
 // create builder
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // build
 var app = builder.Build();
 
@@ -21,10 +23,45 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+var customers = new List<Customer>
+{
+    new Customer(1, "John"),
+    new Customer(2, "Mary"),
+    new Customer(3, "Peter")
+};
 
-// run
+app.MapGet("/customers", () => customers);
+
+app.MapGet("/customers/{id:int}", (int id) => customers
+    .FirstOrDefault(customer => customer.id == id));
+
+app.MapPost("/customers", (Customer customer) =>
+{
+    customers.Add(customer);
+    return customer;
+});
+
+app.MapPut("/customers/{id:int}", (int id, Customer customer) =>
+{
+    var index = customers
+        .FindIndex(cust => cust.id == id);
+    customers[index] = customer;
+    return customer;
+});
+
+app.MapDelete("/customers/{id:int}", (int id) =>
+{
+    var customer = customers
+        .FirstOrDefault(cust => cust.id == id);
+    if (customer is not null)
+        customers.Remove(customer);
+});
+
 app.Run();
+
+public record Customer(int id, string Name);
